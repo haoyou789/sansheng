c245c7aa85d8f84b5f7c044396ad564a3bf7de14
yehua <1967526678@qq.com>
2017-02-27 20:29:25 +0800

    提交一下

--- src/com/atguigu/git/Hello.java
+++ src/com/atguigu/git/Hello.java
@@ -1,12 +1,19 @@
 package com.atguigu.git;
 
+import java.util.Date;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		
 		System.out.println("三生style");
 		System.out.println("susu");
-		// 三生三生十里桃花	
+		Date d = new Date();
+		System.out.println("haha ");
+		
+		
+		
+		
 	}
 
 }
