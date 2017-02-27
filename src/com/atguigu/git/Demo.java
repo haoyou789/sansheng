62a66fe522357ef3ecbdc92e35d29450d38c6126
yehua <1967526678@qq.com>
2017-02-27 21:44:55 +0800

    tijiao

---
+++ src/com/atguigu/git/Demo.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+import org.eclipse.jdt.internal.compiler.batch.Main;
+
+public class Demo {
+
+	
+	public static void main(String[] args) {
+		
+		String s = new String();
+	
+		System.out.println(System.currentTimeMillis());
+	}
+}
